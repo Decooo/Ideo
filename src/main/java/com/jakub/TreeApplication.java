224de7ac8a58566d883fa17678c913e405459ecf
Jakub Kuśnierz <jakubkusnierz@gmail.com>
2018-04-12 15:05:33 +0200

    added config

--- src/main/java/com/jakub/TreeApplication.java
+++ src/main/java/com/jakub/TreeApplication.java
@@ -1,12 +1,49 @@
 package com.jakub;
 
+import com.jakub.config.StageManager;
+import com.jakub.view.FxmlView;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class TreeApplication {
+@EnableJpaRepositories("com.jakub.repository")
+public class TreeApplication extends Application {
+
+	private ConfigurableApplicationContext springContext;
+	private StageManager stageManager;
 
 	public static void main(String[] args) {
-		SpringApplication.run(TreeApplication.class, args);
+		Application.launch(args);
+	}
+
+	@Override
+	public void init() throws Exception{
+		springContext = springBootApplicationContext();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		stageManager = springContext.getBean(StageManager.class, primaryStage);
+		displayInitialScene();
+	}
+
+	@Override
+	public void stop() throws Exception{
+		springContext.close();
+	}
+
+	protected void displayInitialScene(){
+		stageManager.switchScene(FxmlView.TREE);
+	}
+
+	private ConfigurableApplicationContext springBootApplicationContext(){
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(TreeApplication.class);
+		String[] args = getParameters().getRaw().stream().toArray(String[]::new);
+		return builder.run(args);
 	}
 }
---
+++ src/main/java/com/jakub/controller/TreeController.java
@@ -0,0 +1,18 @@
+package com.jakub.controller;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by jakub on 12.04.2018.
+ */
+@Controller
+public class TreeController implements Initializable{
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
